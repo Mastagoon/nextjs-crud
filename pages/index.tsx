9d9195fcd069eb13a19fba7df74805e445b986f0
= <inzilagiway@gmail.com>
2022-04-18 16:18:58 +0300

    initial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,43 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
 
 const Home: NextPage = () => {
+	const [username, setUsername] = useState<string>('')
+	const [password, setPassword] = useState<string>('')
+
+	const handleSubmit = () => {
+
+	}
+
+
+
 	return (
-		<h1 className="text-red-400" >Hi mom</h1>
+		<>
+			<div className="grid grid-cols-12">
+				<div className="col-span-12 py-3 bg-[#7C007C] flex flex-row justify-between">
+					<div className="mx-5 text-white">
+						<h1 className="text-xl font-bold">LOGO</h1>
+					</div>
+					<ul className="flex flex-row justify-between mx-5 text-white">
+						<li className="mx-2">login</li>
+						<li className="mx-2">register</li>
+					</ul>
+				</div>
+			</div>
+			<div className="w-full grid grid-cols-12 align-center">
+				<div className="col-span-4"></div>
+				<div className="flex flex-col bg-[#e1e1e1] items-center my-5 col-span-4">
+					<div className="text-xl font-bold">Signup</div>
+					<hr />
+					<label htmlFor="username">Username</label>
+					<input value={username} onChange={(e) => setUsername(e.target.value)} type="text" className="p-1 w-80" />
+					<label htmlFor="password">Password</label>
+					<input value={password} onChange={e => setPassword(e.target.value)} type="password" className="p-1 w-80" />
+					<button onClick={handleSubmit} className="p-2 my-3 text-white bg-blue-500 rounded-md hover:opacity-80">Register</button>
+				</div>
+				<div className="col-span-4"></div>
+			</div>
+		</>
 	)
 }
 
